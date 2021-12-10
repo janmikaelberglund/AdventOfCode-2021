1128e1a1bae08728f628f9aed78ca36c71cc1e32
Mikael Berglund <Mikael.Berglund@strongpoint.com>
2021-12-10 15:55:13 +0100

    feat: day-7 complete

--- Day-7/Program.cs
+++ Day-7/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day_7
 {
@@ -8,19 +10,79 @@ namespace Day_7
         public static string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(path);
+            var rawInput = File.ReadAllText(path);
 
+            List<int> input = new(Array.ConvertAll(rawInput.Split(','), int.Parse));
 
-            Console.WriteLine(part1(input));
-            Console.WriteLine(part2(input));
+            Console.WriteLine(part1(new(input)));
+            Console.WriteLine(part2(new(input)));
         }
-
-        private static int part2(string[] input)
+        private static int part1(List<int> input)
         {
-            throw new NotImplementedException();
+            input = input.OrderBy(x => x).ToList();
+
+            var fuelCount = int.MaxValue;
+
+            for (int i = 0; i < input.Last(); i++)
+            {
+                var temp = FuelCounter(input, i);
+
+                if (temp < fuelCount)
+                {
+                    fuelCount = temp;
+                }
+            }
+
+
+            return fuelCount;
         }
 
-        private static int part1(string[] input)
+        private static int FuelCounter(List<int> input, int i)
         {
-            throw new NotImplementedException();
+            var horizontalLine = i;
+            var fuelCount = 0;
+            foreach (var sub in input)
+            {
+                fuelCount += Math.Abs(sub - horizontalLine);
+            }
+
+            return fuelCount;
         }
+
+        private static int part2(List<int> input)
+        {
+            input = input.OrderBy(x => x).ToList();
+
+            var fuelCount = int.MaxValue;
+
+            for (int i = 0; i < input.Last(); i++)
+            {
+                var temp = FuelCounter2(input, i);
+
+                if (temp < fuelCount)
+                {
+                    fuelCount = temp;
+                }
+            }
+
+            return fuelCount;
+        }
+
+        private static int FuelCounter2(List<int> input, int i)
+        {
+            var horizontalLine = i;
+            var fuelCount = 0;
+            foreach (var sub in input)
+            {
+                var distance = Math.Abs(sub - horizontalLine);
+                for (int j = 0; j < distance; j++)
+                {
+                    fuelCount += j + 1;
+                }
+
+            }
+
+            return fuelCount;
+        }
+    }
+}
---
+++ Day-9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day_9
+{
+    internal class Program
+    {
+        public static string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+        static void Main(string[] args)
+        {
+            var rawInput = File.ReadAllText(path);
+
+            List<int> input = new(Array.ConvertAll(rawInput.Split(','), int.Parse));
+
+            Console.WriteLine(part1(new(input)));
+            Console.WriteLine(part2(new(input)));
+        }
+        private static int part1(List<int> input)
+        {
+
+
+            return 0;
+        }
+
+        private static int part2(List<int> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
