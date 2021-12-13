7d55a60d6aee125efc8166f1462d244311b9587b
Mikael Berglund <Mikael.Berglund@strongpoint.com>
2021-12-13 15:41:16 +0100

    feat: day 8 part one

--- Day-6/Program.cs
+++ Day-6/Program.cs
@@ -2,34 +2,33 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Numerics;
 
 namespace Day_6
 {
     internal class Program
     {
-        public static string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+        public static string path = Path.Combine(Environment.CurrentDirectory, "example.txt");
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(path);
+            string[] inputRaw = File.ReadAllLines(path);
 
-            var split = input[0].Split(',');
+            var split = inputRaw[0].Split(',');
 
             List<int> inputList = new List<int>(Array.ConvertAll(split, int.Parse));
-            List<List<int>> biglist = new();
 
-            foreach (var inp in inputList)
+            BigInteger bigint = new();
+            var count = 0;
+            foreach (var fish in inputList)
             {
-                biglist.Add(new List<int>() {inp });
+                bigint += part2(fish);
+                GC.Collect();
+                count++;
+                Console.WriteLine(count);
             }
 
-            //System.Numerics.BigInteger bigint = new();
-            //foreach (var fishList in biglist)
-            //{
-            //    bigint += part2(fishList);
-            //}
-
-            //Console.WriteLine(part1(new(inputList)));
-            Console.WriteLine(part2(new List<int>() { 1 }));
+            Console.WriteLine(part1(new(inputList)));
+            Console.WriteLine(bigint);
         }
         private static int part1(List<int> input)
         {
@@ -53,26 +52,102 @@ namespace Day_6
             return input.Count;
         }
 
-        private static int part2(List<int> input)
+        private static BigInteger part2(int input)
         {
+            List<List<int>> temp = new();
+
+            for (int i = 0; i < 30; i++)
+            {
+                temp.Add(new());
+            }
+            temp[0].Add(input);
+
             for (int i = 256; i > 0; i--)
             {
-                int count = input.Count;
-                for (int j = 0; j < count; j++)
+                for (int j = 0; j < temp.Count; j++)
                 {
-                    if (input[j] == 0)
+                    int count = temp[j].Count;
+                    var split = 0;
+                    for (int k = 0; k < count; k++)
                     {
-                        input.Add(8);
-                        input[j] = 6;
-                    }
-                    else
-                    {
-                        input[j]--;
+                        if (temp[j][k] == 0)
+                        {
+                            temp[split].Add(8);
+                            temp[j][k] = 6;
+                        }
+                        else
+                        {
+                            temp[j][k] --;
+                        }
+                        if (split < 29)
+                        {
+                            split++;
+                        }
+                        else
+                        {
+                            split = 0;
+                        }
                     }
                 }
             }
 
-            return input.Count;
+            BigInteger counter = 0;
+            for (int i = 0; i < temp.Count; i++)
+            {
+                counter += temp[i].Count;
+            }
+
+            temp = null;
+            
+            return counter;
+        }
+
+        private static BigInteger part2b(int input)
+        {
+            List<List<int>> temp = new();
+
+            for (int i = 0; i < 15; i++)
+            {
+                temp.Add(new());
+            }
+            temp[0].Add(input);
+
+            for (int i = 256; i > 0; i--)
+            {
+                for (int j = 0; j < temp.Count; j++)
+                {
+                    int count = temp[j].Count;
+                    for (int k = 0; k < temp[j].Count; k++)
+                    {
+                        if (temp[j][k] == 0)
+                        {
+                            if (j == 0)
+                            {
+                                temp[temp.Count - 1].Add(8);
+                            }
+                            else
+                            {
+                                temp[j - 1].Add(8);
+                            }
+                            temp[j][k] = 6;
+                        }
+                        else
+                        {
+                            temp[j][k]--;
+                        }
+                    }
+                }
+            }
+
+            BigInteger counter = 0;
+            for (int i = 0; i < temp.Count; i++)
+            {
+                counter += temp[i].Count;
+            }
+
+            temp = null;
+
+            return counter;
         }
     }
 }
--- Day-8/Program.cs
+++ Day-8/Program.cs
@@ -1,31 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day_8
 {
     internal class Program
     {
-        public static string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
+        public static string path = Path.Combine(Environment.CurrentDirectory, "example.txt");
         static void Main(string[] args)
         {
-            var rawInput = File.ReadAllText(path);
+            var rawInput = File.ReadAllLines(path);
 
-            List<int> input = new(Array.ConvertAll(rawInput.Split(','), int.Parse));
+            List<List<string>> input = new();
+
+            foreach (var rawLine in rawInput)
+            {
+                var split = rawLine.Split(" | ");
+                input.Add(new(split[1].Split(' ')));
+            }
+
 
             Console.WriteLine(part1(new(input)));
             Console.WriteLine(part2(new(input)));
         }
-        private static int part1(List<int> input)
+        private static int part1(List<List<string>> input)
         {
+            var count = 0;
+            foreach (var list in input)
+            {
+                foreach (var str in list)
+                {
+                    var condition = str.Length == 2 || str.Length == 3 || str.Length == 4 || str.Length == 7;
+                    if (condition)
+                    {
+                        count++;
+                    }
+                }
+            }
 
-
-            return 0;
+            return count;
         }
 
-        private static int part2(List<int> input)
+        private static int part2(List<List<string>> input)
         {
-            throw new NotImplementedException();
+            List<int> result = new();
+            foreach (var list in input)
+            {
+                result.Add(DecodeNumber(list));
+            }
+
+
+            return result.Sum(x => x);
+        }
+
+        private static int DecodeNumber(List<string> list)
+        {
+            string result = "";
+
+            string one = list.FirstOrDefault(x => x.Length == 2);
+            string seven = list.FirstOrDefault(x => x.Length == 3);
+            string four = list.FirstOrDefault(x => x.Length == 4);
+            string eight = list.FirstOrDefault(x => x.Length == 7);
+            string zero;
+            string nine;
+            if (four is not null)
+            {
+                string zeroOrSix = list.FirstOrDefault(x => x.Length == 6 && x.Count(y => four.Contains(y)) == 3);
+                if (zeroOrSix is not null)
+                {
+                    zero = zeroOrSix.Count(x => seven.Contains(x)) == 3 ? zeroOrSix : null;
+                }
+                nine = list.FirstOrDefault(x => x.Length == 6 && x.Count(y => four.Contains(y)) == 4);
+            }
+
+            //Finish all conditions
+
+            return int.Parse(result);
         }
     }
 }
